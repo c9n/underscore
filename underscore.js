5b9d541443015a5eb9632da32ad3cec0cf756583
Lynn <homlean@gmail.com>
2015-02-27 17:48:17 +0800

    boom

--- underscore.js
+++ underscore.js
@@ -6,4 +6,45 @@
 
 (function() {
 
+  // Baseline
+  // --------
+
+  var root = this;
+
+  var previousUnderscore = root._;
+
+  var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
+
+  var
+    push = ArrayProto.push,
+    slice = ArrayProto.slice,
+    toString = ArrayProto.toString,
+    hasOwnProperty = ArrayProto.hasOwnProperty;
+
+  var
+    nativeIsArray = Array.isArray,
+    nativeKeys = Array.keys,
+    nativeBind = FuncProto.bind,
+    nativeCreate = Object.create;
+
+  var Ctor = function(){};
+
+  var _ = function(obj) {
+    if (obj instanceof _) return obj;
+    if (!(this instanceof _)) return new _(obj);
+    this._wrapped = obj;
+  };
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = _;
+    }
+    exports._ = _;
+
+  } else {
+    root._ = _;
+  }
+
+  _.VERSION = 'NIL';
+
 }.call(this));
