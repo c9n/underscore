42f53a170233164ec606429acb68dbff910e222a
Lynn <homlean@gmail.com>
2015-02-28 12:14:26 +0800

    is array-like func

--- underscore.js
+++ underscore.js
@@ -70,4 +70,10 @@
     };
   };
 
+  var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+  var isArrayLike = function(collection) {
+    var length = collection != null && collection.length;
+    return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+  };
+
 }.call(this));
